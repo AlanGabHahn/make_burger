243037ea9d765387742df23be98eff5217339a25
Alan <alan_hahn1995@outlook.com>
2024-02-27 08:39:49 -0300

    Implementando Vue router

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>Footer</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Footer"
+}
+</script>
+
+<style>
+    
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav>
+        <router-link to="/">
+            <img src="" id="logo">
+        </router-link>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/pedidos">Pedidos</router-link>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
