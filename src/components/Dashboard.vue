198a492b56e5d6532a7e48c61d60bb204cf2edc0
Alan <alan_hahn1995@outlook.com>
2024-03-06 08:51:57 -0300

    Resgatando pedidos do banco

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -10,14 +10,16 @@
                 <div>Ações:</div>
             </div>
             <div id="burger-table-rows">
-                <div class="burger-table-row">
-                    <div class="order-number">1</div>
-                    <div>Nome</div>
-                    <div>Pao</div>
-                    <div>Carne</div>
+                <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+                    <div class="order-number">{{  burger.id }}</div>
+                    <div>{{ burger.nome }}</div>
+                    <div>{{ burger.pao }}</div>
+                    <div>{{ burger.carne }}</div>
                     <div>
                         <ul>
-                            <li>opcionais</li>
+                            <li v-for="(opcional, index) in burger.opcionais" :key="index">
+                                {{ opcional }}
+                            </li>
                         </ul>
                     </div>
                     <div>
@@ -34,7 +36,25 @@
 
 <script>
 export default {
-    name: "Dashboard"
+    name: "Dashboard",
+    data() {
+        return {
+            burgers: null,
+            burger_id: null,
+            status: []
+        }
+    },
+    methods: {
+        async getPedidos() {
+            const req = await fetch("http://localhost:3000/burgers");
+            const data = await req.json();
+            this.burgers = data;
+
+        }
+    },
+    mounted() {
+        this.getPedidos();
+    },
 }
 </script>
 
@@ -82,7 +102,7 @@ export default {
         cursor: pointer;
         transition: .5s;
     }
-    .delete-btn:houver {
+    .delete-btn:hover {
         background-color: transparent;
         color: #222;
     }
