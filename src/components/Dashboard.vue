eae29202c5e2103ee5fbd74c9247268f87438ccf
Alan <alan_hahn1995@outlook.com>
2024-03-07 08:51:22 -0300

    Atualização de pedidos

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -23,12 +23,12 @@
                         </ul>
                     </div>
                     <div>
-                        <select name="status" class="status">
+                        <select name="status" class="status" @change="updateBurger($event, burger.id)">
                             <option value="">Selecione</option>
                             <option 
                                 v-for="s in status" 
                                 :key="s.id" 
-                                value="s.tipo" 
+                                :value="s.tipo" 
                                 :selected="burger.status == s.tipo"
                             >
                                 {{ s.tipo }}
@@ -70,6 +70,17 @@ export default {
             });
             const res = await req.json();
             this.getPedidos();
+        },
+        async updateBurger(event, id) {
+            const option = event.target.value;
+            const dataJson = JSON.stringify({ status: option });
+            const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+                method: "PATCH",
+                headers: { "Content-Type" : "application/json" },
+                body: dataJson
+            });
+            const res = await req.json();
+            console.log(res);
         }
     },
     mounted() {
